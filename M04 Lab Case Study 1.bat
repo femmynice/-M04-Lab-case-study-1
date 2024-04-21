cc4766335d0f6b3a7b9615799f2da472b6ca3e3a
femmynice <157413249+femmynice@users.noreply.github.com>
2024-04-21 13:35:52 -0400

    Add files via upload

---
+++ M04 Lab Case Study 1.bat
@@ -0,0 +1,12 @@
+""" 
+Author: Adefemi Adegite
+Date written: 04/21/24 
+Assignment: M04 Lab-Case Study
+ """
+def hours():
+     print('Open 9-5 daily')
+import zoo
+     zoo.hours()
+>>> import zoo
+>>> zoo.hours()
+open 9-5 daily
